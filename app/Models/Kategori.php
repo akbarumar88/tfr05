caa7c2d33b387ab20be8bb163be71d367c907250
Akbar Umar Alfaro <akbarumar88@gmail.com>
2022-03-12 10:56:00 +0700

    Initial Commit

---
+++ app/Models/Kategori.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Kategori
+{
+    // use HasFactory;
+
+    private static $kategori = [
+        [
+            'id' => 1,
+            'kategori' => 'Makanan'
+        ],
+        [
+            'id' => 2,
+            'kategori' => 'Minuman'
+        ],
+        [
+            'id' => 3,
+            'kategori' => 'Jajan'
+        ],
+    ];
+
+    public static function all()
+    {
+        return collect(self::$kategori);
+    }
+
+    public static function find($id)
+    {
+        $kategori = static::all();
+        return $kategori->first();
+    }
+}
